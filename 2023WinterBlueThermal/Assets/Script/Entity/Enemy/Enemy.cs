624cd707930f0240a6e483e168ab582d920f2230
re-trograde <70553990+re-trograde@users.noreply.github.com>
2023-12-24 15:03:47 +0900

    Revert "Revert "Merge branch 'Entity' into Weapon""
    This reverts commit 4d7998da600eac3603d7ee67c411d1af0f9e82eb.

---
+++ 2023WinterBlueThermal/Assets/Script/Entity/Enemy/Enemy.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Enemy : Entity
+{
+    [HideInInspector]
+    public bool _onExecution = false;    //처형 발생 여부
+    
+    [SerializeField]
+    protected int _attackDamage;   //공격력
+
+    [Header("ChasingAndScattering")]
+    [SerializeField] private float _chasingTime;  //추격 시간
+    [SerializeField] private float _scatteringTime;   //흩어짐 시간
+    [SerializeField] private float _scatteringRange;
+    [SerializeField] private Transform _chasingTarget;
+
+    [Header("Attack")]
+    [SerializeField] private float _attackRange;
+    [SerializeField] private float _readyToAttackTime;
+    [SerializeField] private float _attackDelayTime;
+    private float _currentAttackTime;
+
+    private NavMeshAgent _agent;
+
+    //=========================================================================================================================
+
+    protected abstract void Attack(Transform chasingTarget);
+
+    private IEnumerator DoChaseAndScatter()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(Chase(_agent, _chasingTarget, _chasingTime));
+            yield return StartCoroutine(Scatter(_agent, _scatteringTime, _scatteringRange));
+        }
+    }
+
+    protected abstract IEnumerator Chase(NavMeshAgent agent, Transform chasingTarget, float chasingTime);
+
+    protected abstract IEnumerator Scatter(NavMeshAgent agent, float scatteringTime, float scatteringRange);
+
+    protected abstract bool RandomPoint(float range, out Vector3 result);
+    
+    protected override void Dead() //죽음
+    {
+        if (_currentHp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DoChaseAndScatter());
+    }
+
+    private void Update()
+    {
+        PrepareToAttack();
+    }
+
+    private void PrepareToAttack()
+    {
+        float distanceToPlayer = Vector3.Distance(gameObject.transform.position, _chasingTarget.transform.position);
+
+        if (distanceToPlayer < _attackRange)
+        {
+            _currentAttackTime += Time.deltaTime;
+        }
+        else
+        {
+            _currentAttackTime = 0;
+        }
+
+        if (_currentAttackTime >= _readyToAttackTime)
+        {
+            DoAttack();
+            _currentAttackTime = _readyToAttackTime - _attackDelayTime;
+        }
+    }    
+
+    private void DoAttack()
+    {
+        Attack(_chasingTarget);
+    }
+
+    private void DropAmmoOnField()  //처형 시 탄약 떨굼
+    {
+        //추후 총기 및 총알 제작 후 총알 떨구는 로직 추가
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            //추후에 총기 및 총알 제작후 DecreaseHP() 호출하면서
+            //파라미터로 총알 데미지 넘겨줌
+        }
+    }
+}
